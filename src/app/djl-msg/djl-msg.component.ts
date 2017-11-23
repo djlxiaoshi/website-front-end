63fd349f55e6f695ffe25bf4859d86b393125980
DJL箫氏 <1281233206@qq.com>
2017-11-23 21:21:16 +0800

    加入tooltips提示工具

--- src/app/djl-msg/djl-msg.component.ts
+++ src/app/djl-msg/djl-msg.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angu
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import Tooltips from '../shared/utils/izitoast.util';
+const swal = require('sweetalert');
+
 
 @Component({
   selector: 'app-djl-msg',
@@ -54,7 +56,22 @@ export class DjlMsgComponent implements OnInit {
     debugger
   }
 
-  signUp() {}
+  signUp() {
+    swal({
+      title: '确定要退出登录吗？',
+      text: 'Once deleted, you will not be able to recover this imaginary file!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal('aready exit', {
+          icon: 'success',
+        });
+      }
+    });
+  }
 
   closeMsgDialog() {
     this.msgVisibleChange.emit(false);
